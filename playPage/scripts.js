64301e1ed0fe95c47ab7e99e3f26860ee7305a79
Arthur Pimentel <art.ol.pimentel@gmail.com>
2024-12-21 15:14:46 -0300

    Animação "Abre e Fecha" final
    Finalização das animações de abrir e fechar os pergaminhos (Abas) da página

--- playPage/scripts.js
+++ playPage/scripts.js
@@ -3,15 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     largeScrolls.forEach((scroll) => {
         scroll.addEventListener("click", () => {
-            const isActive = scroll.classList.contains("active");
-
             largeScrolls.forEach((otherScroll) => {
                 if (otherScroll !== scroll) {
-                    otherScroll.style.display = isActive ? "block" : "none";
+                    otherScroll.classList.add("hidden");
                     otherScroll.classList.remove("active");
                 }
             });
+
             scroll.classList.toggle("active");
+            
+            if (!scroll.classList.contains("active")) {
+                largeScrolls.forEach((otherScroll) => {
+                    otherScroll.classList.remove("hidden");
+                })
+            }
         });
     });
 });
