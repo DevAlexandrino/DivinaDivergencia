a62077cc81c63d36b196d80e96f4cac55079dc1a
Arthur Siqueira Campos Alexandrino <devalexandrino@gmail.com>
2024-12-22 17:31:20 -0300

    adicionando objeto personagem e pasta para as funções

---
+++ playPage/personagem.js
@@ -0,0 +1,30 @@
+const personagem = {
+    dados: {
+        nome: "",
+        raca: "",
+        classe: "",
+        nivel: 1,
+        experiencia: 0,
+    },
+    caracteristicas: {
+        vidaMaxima: 0,
+        vidaAtual: 0,
+        manaMaxima: 0,
+        manaAtual: 0,
+        vigorMaximo: 0,
+        vigorAtual:0,
+        aptidao: "",
+        genetica: "",
+    },
+    atributos: {
+        iniciativa: 0,
+        forca: 0,
+        precisao: 0,
+        vontade: 0,
+        furtividade: 0,
+        inteligencia: 0,
+        atencao: 0,
+        atletismo: 0,
+        percepcaoPassivaBonus: 0,
+    }
+}
